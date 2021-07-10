d61aa85c14df6b14b5151b8a452f8b20b1abaab7
Felix <felixhmngo@gmail.com>
2021-07-11 00:42:33 +1000

    completed basic navbar prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, {useState} from 'react';
 import './App.css';
-import OrderPage from './pages/OrderPage.js';
+// import OrderPage from './pages/OrderPage.js';
+import Navbar from './components/Navbar.js';
 
 function App() {
+  const [user, setUser] = useState({
+    email: "example@email.com",
+    firstName: "Bob",
+    lastName: "Smith",
+    membershipPoints: 15
+  })
   return (
     <div className="App">
-      
+      {/* <OrderPage /> */}
+      <Navbar user={user}/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Navbar.css';
+import logo from '../imgs/logo.png';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {IoCloseSharp} from 'react-icons/io5';
 
-export default function Navbar () {
+export default function Navbar (props) {
+    const [navbar, setNavbar] = useState(true)
+
+    window.addEventListener('resize', () => (window.innerWidth <= 500) ? setNavbar(false) : setNavbar(true))
+    console.log(props)
     return(
         <nav>
-            
+            <div className='nav-container'>
+                <div className='logo-container'>
+                    <img className='logo' src={logo} alt='KON Ramen Logo' />
+                    <button className='menu-btn' onClick={() => setNavbar(!navbar)}>
+                        { navbar ? <IoCloseSharp /> : <GiHamburgerMenu /> }
+                    </button>
+                    {navbar}
+                </div>
+                <div className='nav-links-container'>
+                    <ul className={navbar ? 'nav-links' : 'nav-links hide-nav'}>
+                        <li className='nav-link'>Hi, {props.user.firstName} !</li>
+                        <li className='nav-link'>Membership Points : {props.user.membershipPoints}</li>
+                        <li className='nav-link'><a>Edit Profile</a></li>
+                        <li className='nav-link'><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
